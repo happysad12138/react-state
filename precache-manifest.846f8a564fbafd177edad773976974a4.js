7123d50db3051cddae5b79f3058a11fc0fc6c2a6
happysad12138 <1148361751@qq.com>
2019-05-28 09:08:46 +0800

    add-react

---
+++ precache-manifest.846f8a564fbafd177edad773976974a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd9dd30d5a91191a3d40",
+    "url": "/static/js/main.225231d2.chunk.js"
+  },
+  {
+    "revision": "69f089c75d6c6b0b1a92",
+    "url": "/static/js/2.ad7bd639.chunk.js"
+  },
+  {
+    "revision": "bd9dd30d5a91191a3d40",
+    "url": "/static/css/main.ceda97cc.chunk.css"
+  },
+  {
+    "revision": "2a0b34ed7f4364190716931e74c19aa7",
+    "url": "/index.html"
+  }
+];
